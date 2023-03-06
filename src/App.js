73f820ae4b23e84aa808e98a7d9088b5a229bb48
Joaofsrs <rsjoao12@gmail.com>
2023-03-06 04:57:45 -0300

    Tela de Inicio(Bonus)

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import styled from 'styled-components';
 import Header from "./components/Header";
 import ListaCards from "./components/ListaCards";
 import Footer from './components/Footer';
+import TelaInicio from './components/TelaInicio';
 import React from 'react';
 
 export default function App() {
     const [quantidadeRespostas, setQuantidade] = React.useState(0);
+    const [iniciou, setInicio] = React.useState(false);
 
     function addQuantidade(){
         setQuantidade(quantidadeRespostas+1);
     }
 
-    return (
-        <ContainerAdd>
-            <Header></Header>
-            <ListaCards addQuantidade={addQuantidade}></ListaCards>
-            <Footer quantidadeRespostas={quantidadeRespostas} ></Footer>
-        </ContainerAdd>
-    );
+    if(!iniciou){
+        return (
+            <TelaInicio setInicio={setInicio} />
+        );
+    }else if(iniciou){
+        return (
+            <ContainerApp>
+                <Header/>
+                <ListaCards addQuantidade={addQuantidade} />
+                <Footer quantidadeRespostas={quantidadeRespostas} />
+            </ContainerApp>
+        );
+    }
 }
 
-const ContainerAdd = styled.div`
+const ContainerApp = styled.div`
     width: 100vw;
     height: 100%;
     margin-top: 50px;
---
+++ src/components/TelaInicio.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { css } from 'styled-components';
+import logo from '../assets/logo.png';
+
+export default function TelaInicio(props){
+    return (
+        <ContainerInicio>
+            <img src={logo} alt="Logo de site" />
+            <h1>ZapRecall</h1>
+            <button onClick={props.setInicio}>Iniciar Recall!</button>
+        </ContainerInicio>
+    );
+}
+
+
+const RighteousFont = css`
+    @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+`;
+
+const ContainerInicio = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    img{
+        height: 161px;
+        width: 136px;
+        border-radius: 0px;
+        margin-bottom: 20px;
+    }
+    h1{
+        font-family: Righteous;
+        font-size: 36px;
+        font-weight: 400;
+        letter-spacing: -0.012em;
+        margin-bottom: 20px;
+    }
+    button{
+        height: 54px;
+        width: 246px;
+        border-radius: 5px;
+        background-color: #FFFFFF;
+        color: #D70900;
+        border: none;
+    }
+`;
